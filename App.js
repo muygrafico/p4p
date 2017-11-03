924ddb87062f3594049a74c12cc97e843b3169eb
German Hernandez <muygrafico@gmail.com>
2017-11-03 15:46:42 -0500

    add image upload

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import AWS from 'aws-sdk';
+import RNFetchBlob from 'react-native-fetch-blob';
+import { Buffer } from 'buffer';
+
 import { WithAPI } from './lib/Categories/API/Components';
 import { WithAuth } from './lib/Categories/Auth/Components';
 import { WithStorage } from './lib/Categories/Storage/Components';
-
 import { SignIn, SignUp } from './lib/Categories/Auth/Components/Examples';
 import awsmobile from './aws-exports';
 
@@ -13,6 +16,38 @@ export default WithStorage(WithAPI(WithAuth(class App extends React.Component {
   state = {
     objectUrl: null,
     apiResponse: null
+  }
+
+  async handleUploadFile() {
+    const url = 'https://awsmedia.s3.amazonaws.com/AWS_Logo_PoweredBy_127px.png';
+    const [, fileName, extension] = /.*\/(.+)\.(\w+)$/.exec(url);
+
+    // Get cognito identity for the signed in user
+    const { IdentityId } = AWS.config.credentials.data;
+
+    // File will be uploaded to the user's private space in the S3 bucket
+    const key = `private/${IdentityId}/${fileName}`;
+
+    let objectUrl = null;
+
+    try {
+      // Download file from the internet.
+      const download = await RNFetchBlob.fetch('GET', url);
+      const { data } = download;
+      const { respInfo: { headers: { 'Content-Type': contentType } } } = download;
+
+      // Upload the file
+      const upload = await this.props.storage.putObject(key, new Buffer(data, 'base64'), contentType);
+
+      // Get url for stored object. This is an S3 presigned url. See: http://docs.aws.amazon.com/AmazonS3/latest/dev/ShareObjectPreSignedURL.html
+      objectUrl = this.props.storage.getObjectUrl(upload.key);
+
+      console.log(objectUrl);
+    } catch (err) {
+      console.warn(err);
+    }
+
+    this.setState({ objectUrl });
   }
 
   async handleCallAPI() {
@@ -48,6 +83,8 @@ export default WithStorage(WithAPI(WithAuth(class App extends React.Component {
     return (
       session ?
      (<View style={styles.container}>
+       {this.state.objectUrl && <Image source={{ uri: this.state.objectUrl }} style={{width: 200, height: 200, resizeMode: 'contain'}} />}
+       <Button title="Upload file" onPress={this.handleUploadFile.bind(this)} />
        <Button title="Call API" onPress={this.handleCallAPI.bind(this)} />
        <Text>Response: {this.state.apiResponse && this.handleResponse(this.state.apiResponse)}</Text>
        <Button title="Sign Out" onPress={() => this.props.doSignOut()} />
