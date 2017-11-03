2a17bcf0666b700a0dbd34458665bd0e1ec8ff15
German Hernandez <muygrafico@gmail.com>
2017-11-03 15:28:18 -0500

    update app to connect and call backend

--- App.js
+++ App.js
@@ -1,17 +1,66 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { WithAPI } from './lib/Categories/API/Components';
+import { WithAuth } from './lib/Categories/Auth/Components';
+import { WithStorage } from './lib/Categories/Storage/Components';
 
-export default class App extends React.Component {
+import { SignIn, SignUp } from './lib/Categories/Auth/Components/Examples';
+import awsmobile from './aws-exports';
+
+
+export default WithStorage(WithAPI(WithAuth(class App extends React.Component {
+
+  state = {
+    objectUrl: null,
+    apiResponse: null
+  }
+
+  async handleCallAPI() {
+    const { api } = this.props;
+
+    // Get endpoint
+    const cloudLogicArray = JSON.parse(awsmobile.aws_cloud_logic_custom);
+    const endPoint = cloudLogicArray[0].endpoint;
+
+    const requestParams = {
+      method: 'GET',
+      url: endPoint + '/items/pets',
+    };
+
+    let apiResponse = null;
+
+    try {
+      apiResponse = await api.restRequest(requestParams);
+    } catch (err) {
+      console.warn(err);
+    }
+
+    this.setState({ apiResponse });
+  }
+
+  handleResponse(response) {
+    return JSON.stringify(response)
+  }
+
   render() {
+    const { session } = this.props;
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      session ?
+     (<View style={styles.container}>
+       <Button title="Call API" onPress={this.handleCallAPI.bind(this)} />
+       <Text>Response: {this.state.apiResponse && this.handleResponse(this.state.apiResponse)}</Text>
+       <Button title="Sign Out" onPress={() => this.props.doSignOut()} />
+     </View>)
+     :
+     (<View style={styles.container}>
+       <SignIn {...this.props} />
+       <SignUp {...this.props} />
+     </View>)
     );
   }
-}
+})));
+
 
 const styles = StyleSheet.create({
   container: {
