b7f3d74bf1d785c236717d1fc2d589de3a2df500
German Hernandez <muygrafico@gmail.com>
2017-11-06 20:23:49 -0500

    take picture | preview taken picture

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+// import { DrawerNavigator } from 'react-navigation';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image } from 'react-native';
 import AWS from 'aws-sdk';
 import RNFetchBlob from 'react-native-fetch-blob';
 import { Buffer } from 'buffer';
-
 import { WithAPI } from './lib/Categories/API/Components';
 import { WithAuth } from './lib/Categories/Auth/Components';
 import { WithStorage } from './lib/Categories/Storage/Components';
 import { AutoSignIn, SignUp } from './lib/Categories/Auth/Components/Examples';
 import awsmobile from './aws-exports';
-
+import Camera from 'react-native-camera';
 
 export default WithStorage(WithAPI(WithAuth(class App extends React.Component {
 
   state = {
     objectUrl: null,
-    apiResponse: null
+    apiResponse: null,
+    showPreview: false,
+    imageData: null
   }
 
   async handleUploadFile() {
@@ -79,18 +86,54 @@ export default WithStorage(WithAPI(WithAuth(class App extends React.Component {
     return JSON.stringify(response)
   }
 
+  takePicture() {
+    const options = {};
+    //options.location = ...
+    this.camera.capture({metadata: options})
+    .then((data) => {
+      console.log(data)
+      console.log(data.temp);
+      this.setState({
+        showPreview: true,
+        imageURL: data.path
+      })
+    })
+    .catch(err => console.error(err));
+  }
+
   render() {
     const { session } = this.props;
 
     return (
       session ?
-     (<View style={styles.container}>
-       {this.state.objectUrl && <Image source={{ uri: this.state.objectUrl }} style={{width: 200, height: 200, resizeMode: 'contain'}} />}
-       <Button title="Upload file" onPress={this.handleUploadFile.bind(this)} />
-       <Button title="Call API" onPress={this.handleCallAPI.bind(this)} />
-       <Text>Response: {this.state.apiResponse && this.handleResponse(this.state.apiResponse)}</Text>
-       {/* <Button title="Sign Out" onPress={() => this.props.doSignOut()} /> */}
-     </View>)
+    //  (<View style={styles.container}>
+    //    {this.state.objectUrl && <Image source={{ uri: this.state.objectUrl }} style={{width: 200, height: 200, resizeMode: 'contain'}} />}
+    //    <Button title="Upload file" onPress={this.handleUploadFile.bind(this)} />
+    //    <Button title="Call API" onPress={this.handleCallAPI.bind(this)} />
+    //    <Text>Response: {this.state.apiResponse && this.handleResponse(this.state.apiResponse)}</Text>
+    //    {/* <Button title="Sign Out" onPress={() => this.props.doSignOut()} /> */}
+    //  </View>)
+    (
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <View style={styles2.container}>
+            {!this.state.showPreview && <Camera
+              ref={(cam) => {
+                this.camera = cam;
+              }}
+              style={styles2.preview}
+              captureTarget={Camera.constants.CaptureTarget.disk}
+              aspect={Camera.constants.Aspect.fill}>
+
+              <Button style={styles2.capture} title="Capture" onPress={this.takePicture.bind(this)} />
+            </Camera>}
+            {this.state.showPreview &&  <Image
+                source={{uri: this.state.imageURL, isStatic:true}}
+                style={styles.canvas}
+                />
+              }
+          </View>
+        </View>
+    )
      :
      (<View style={styles.container}>
        {/* <SignIn {...this.props} /> */}
@@ -108,5 +151,35 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    position: 'relative',
+    padding: 20
   },
+  canvas: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+});
+
+const styles2 = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40,
+    marginBottom: 200
+  }
 });
