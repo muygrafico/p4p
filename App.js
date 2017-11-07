f91ebe4e6a7292de888b6e7c0f98e8036f1a19c4
German Hernandez <muygrafico@gmail.com>
2017-11-07 11:08:07 -0500

    styling wip (not that fancy code, making it work, clean after)

--- App.js
+++ App.js
@@ -5,8 +5,10 @@ import {
   Text,
   View,
   Button,
+  TouchableOpacity,
   Image } from 'react-native';
 import AWS from 'aws-sdk';
+import { Icon } from 'react-native-elements';
 import RNFetchBlob from 'react-native-fetch-blob';
 import { Buffer } from 'buffer';
 import { WithAPI } from './lib/Categories/API/Components';
@@ -15,6 +17,8 @@ import { WithStorage } from './lib/Categories/Storage/Components';
 import { AutoSignIn, SignUp } from './lib/Categories/Auth/Components/Examples';
 import awsmobile from './aws-exports';
 import Camera from 'react-native-camera';
+import { TimelineLite } from 'gsap';
+
 
 export default WithStorage(WithAPI(WithAuth(class App extends React.Component {
 
@@ -28,29 +32,21 @@ export default WithStorage(WithAPI(WithAuth(class App extends React.Component {
   async handleUploadFile() {
     // const url = 'https://awsmedia.s3.amazonaws.com/AWS_Logo_PoweredBy_127px.png';
     const url = 'https://www.parrolabs.com/assets/images/posts/react-native.png';
-
     const [, fileName, extension] = /.*\/(.+)\.(\w+)$/.exec(url);
-
     // Get cognito identity for the signed in user
     const { IdentityId } = AWS.config.credentials.data;
-
     // File will be uploaded to the user's private space in the S3 bucket
     const key = `private/${IdentityId}/${fileName}`;
-
     let objectUrl = null;
-
     try {
       // Download file from the internet.
       const download = await RNFetchBlob.fetch('GET', url);
       const { data } = download;
       const { respInfo: { headers: { 'Content-Type': contentType } } } = download;
-
       // Upload the file
       const upload = await this.props.storage.putObject(key, new Buffer(data, 'base64'), contentType);
-
       // Get url for stored object. This is an S3 presigned url. See: http://docs.aws.amazon.com/AmazonS3/latest/dev/ShareObjectPreSignedURL.html
       objectUrl = this.props.storage.getObjectUrl(upload.key);
-
       console.log(objectUrl);
     } catch (err) {
       console.warn(err);
@@ -61,11 +57,8 @@ export default WithStorage(WithAPI(WithAuth(class App extends React.Component {
 
   async handleCallAPI() {
     const { api } = this.props;
-
-    // Get endpoint
-    const cloudLogicArray = JSON.parse(awsmobile.aws_cloud_logic_custom);
+    const cloudLogicArray = JSON.parse(awsmobile.aws_cloud_logic_custom); // Get endpoint
     const endPoint = cloudLogicArray[0].endpoint;
-
     const requestParams = {
       method: 'GET',
       url: endPoint + '/items/pets',
@@ -97,6 +90,7 @@ export default WithStorage(WithAPI(WithAuth(class App extends React.Component {
         showPreview: true,
         imageURL: data.path
       })
+      this.animate
     })
     .catch(err => console.error(err));
   }
@@ -106,39 +100,49 @@ export default WithStorage(WithAPI(WithAuth(class App extends React.Component {
 
     return (
       session ?
-    //  (<View style={styles.container}>
-    //    {this.state.objectUrl && <Image source={{ uri: this.state.objectUrl }} style={{width: 200, height: 200, resizeMode: 'contain'}} />}
-    //    <Button title="Upload file" onPress={this.handleUploadFile.bind(this)} />
-    //    <Button title="Call API" onPress={this.handleCallAPI.bind(this)} />
-    //    <Text>Response: {this.state.apiResponse && this.handleResponse(this.state.apiResponse)}</Text>
-    //    {/* <Button title="Sign Out" onPress={() => this.props.doSignOut()} /> */}
-    //  </View>)
     (
-      <View style={{flex: 1, flexDirection: 'row'}}>
-        <View style={styles2.container}>
-            {!this.state.showPreview && <Camera
-              ref={(cam) => {
-                this.camera = cam;
-              }}
-              style={styles2.preview}
-              captureTarget={Camera.constants.CaptureTarget.disk}
-              aspect={Camera.constants.Aspect.fill}>
-
-              <Button style={styles2.capture} title="Capture" onPress={this.takePicture.bind(this)} />
-            </Camera>}
-            {this.state.showPreview &&  <Image
-                source={{uri: this.state.imageURL, isStatic:true}}
-                style={styles.canvas}
-                />
-              }
+      <View style={styles.appContainer}>
+        <View style={styles.livePreviewContainer}>
+          {!this.state.showPreview &&
+            <View style={styles.livePreview}>
+              <Camera
+                ref={cam => this.camera = cam}
+                style={styles.camera}
+                captureTarget={Camera.constants.CaptureTarget.disk}
+                aspect={Camera.constants.Aspect.fill}>
+              </Camera>
+          </View>
+          }
+          {this.state.showPreview &&
+            <Image
+              source={{uri: this.state.imageURL, isStatic:true}}
+              style={styles.livePreview}
+            />
+          }
+            <View style={styles.bottomBar}>
+              
+              <TouchableOpacity
+                style={{
+                    borderWidth:1,
+                    borderColor:'rgba(0,0,0,0.2)',
+                    alignItems:'center',
+                    justifyContent:'center',
+                    width:50,
+                    height:50,
+                    backgroundColor:'#fff',
+                    borderRadius:50,
+                  }}
+                  onPress={this.takePicture.bind(this)}
+              >
+                <Icon name={"chevron-right"}  size={30} color="#01a699" />
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
     )
      :
      (<View style={styles.container}>
-       {/* <SignIn {...this.props} /> */}
        <AutoSignIn {...this.props} />
-       {/* <SignUp {...this.props} /> */}
      </View>)
     );
   }
@@ -146,40 +150,59 @@ export default WithStorage(WithAPI(WithAuth(class App extends React.Component {
 
 
 const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    backgroundColor: 'blue',
+  },
+  livePreviewContainer: {
+    backgroundColor: '#fff',
+    width:'100%',
+    height:'100%',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
     position: 'relative',
-    padding: 20
-  },
-  canvas: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-  },
-});
+    padding: 20,
 
-const styles2 = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
   },
-  preview: {
+  livePreview: {
+    flex: 1,
+    paddingTop: 25,
+    paddingBottom: 15,
+    paddingLeft: 15,
+    paddingRight: 15
+  },
+  camera: {
     flex: 1,
     justifyContent: 'flex-end',
-    alignItems: 'center'
+    alignItems: 'center',
+    paddingTop: 25,
+    paddingBottom: 15,
+    paddingLeft: 15,
+    paddingRight: 15
   },
-  capture: {
+  captureButton: {
     flex: 0,
     backgroundColor: '#fff',
-    borderRadius: 5,
+    borderRadius: 50,
     color: '#000',
     padding: 10,
     margin: 40,
     marginBottom: 200
+  },
+  bottomBar: {
+    flex: 0.15,
+    borderTopColor: 'white',
+    borderTopWidth: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#282828',
+    bottom: 0,
+    height: 80
   }
 });
