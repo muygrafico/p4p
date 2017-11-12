03005f8e934cd14f6842295615115a0f56bb6a37
German Hernandez <muygrafico@gmail.com>
2017-11-12 13:54:12 -0500

    connect store to debugger

--- configureStore.js
+++ configureStore.js
@@ -3,6 +3,10 @@ import app from './reducers'
 import thunk from 'redux-thunk'
 
 export default function configureStore() {
-  let store = createStore(app, applyMiddleware(thunk))
+  let store = createStore(
+    app,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk)
+  )
   return store
 }
