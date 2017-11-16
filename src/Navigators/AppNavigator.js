ae668bdb335277c43b391461775a0cc25b5e2313
German Hernandez <muygrafico@gmail.com>
2017-11-16 15:54:21 -0500

    set navigation from bottom down for 'QueueList' Screen

--- src/Navigators/AppNavigator.js
+++ src/Navigators/AppNavigator.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { addNavigationHelpers, StackNavigator } from 'react-navigation';
+import {
+  addNavigationHelpers,
+  StackNavigator
+} from 'react-navigation';
+import CardStackStyleInterpolator from 'react-navigation/src/views/CardStack/CardStackStyleInterpolator';
 
 import HomeScreen from '../Screens/Home';
 import QueueListScreen from '../Screens/QueueList';
@@ -11,28 +15,37 @@ import { WithAPI } from '../lib/Categories/API/Components';
 import { WithAuth } from '../lib/Categories/Auth/Components';
 import { WithStorage } from '../lib/Categories/Storage/Components';
 
-export const AppNavigator = StackNavigator(
-  {
-    Home: {
-      screen: HomeScreen,
-      navigationOptions: {header: null}
+export const AppNavigator = StackNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {header: null}
+  },
+  QueueList: {
+    screen: QueueListScreen,
+    navigationOptions: {header: null}
+  },
+  Autologin: {
+    screen: AutoLoginScreen,
+    navigationOptions: {header: null}
+  },
+  Splash: {
+    screen: SplashScreen,
+    navigationOptions: {header: null}
+  },
+},{
+  header: { visible: false },
+  transitionConfig: () => ({
+    screenInterpolator: (sceneProps) => {
+
+      if (sceneProps.scene.route.routeName === 'QueueList')
+        return CardStackStyleInterpolator.forVertical(sceneProps)
+
+      // Otherwise, use the usual horizontal animation
+      if (sceneProps.scene.route.routeName !== 'QueueList')
+        return CardStackStyleInterpolator.forHorizontal(sceneProps)
     },
-    QueueList: {
-      screen: QueueListScreen,
-      navigationOptions: {header: null}
-    },
-    Autologin: {
-      screen: AutoLoginScreen,
-      navigationOptions: {header: null}
-    },
-    Splash: {
-      screen: SplashScreen,
-      navigationOptions: {header: null}
-    },
-  },{
-    header: { visible: false }
-  }
-);
+  }),
+});
 
 const AppWithNavigationState = ({ dispatch, nav }) => (
   <AppNavigator navigation={addNavigationHelpers({ dispatch, state: nav })} />
